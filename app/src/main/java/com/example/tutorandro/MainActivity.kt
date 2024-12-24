36dcddc808f8d8c55585f39959e4e1f224de5100
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-12-24 20:33:38 +0700

    pertemuan 24 desember 2024

--- app/src/main/java/com/example/tutorandro/MainActivity.kt
+++ app/src/main/java/com/example/tutorandro/MainActivity.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -22,8 +21,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tutorandro.ui.theme.TutorAndroTheme
 
@@ -32,6 +31,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             TutorAndroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize(),
@@ -55,13 +55,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContohColumn(nama: String, pesan: String, modifier: Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Hello $nama", style = MaterialTheme.typography.bodyLarge)
+    Column(modifier = modifier) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(16.dp)
+                .border(1.dp, Color.Gray),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Nama: $nama")
+                Text(text = "Pesan: $pesan")
+            }
+        }
 
-        Text(text = pesan, style = MaterialTheme.typography.bodyLarge)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(16.dp)
+                .border(1.dp, Color.Gray),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Nama: $nama")
+                Text(text = "Pesan: $pesan")
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tutorandro/ui/theme/Theme.kt
+++ app/src/main/java/com/example/tutorandro/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.tutorandro.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -18,9 +17,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Yellow30,
+    secondary = Yellow40,
+    tertiary = Yellow50
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
